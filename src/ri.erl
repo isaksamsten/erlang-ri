c037bb457b8b934c582b7a57d1a5c760ab2e522c
Isak Karlsson <isak.karlsson@gmail.com>
2013-01-02 14:49:21 +0100

    fixed semantic vectors

--- src/ri.erl
+++ src/ri.erl
@@ -3,77 +3,78 @@
 
 init() ->
     ets:new(index_vectors, [public, named_table, {write_concurrency, true}, {read_concurrency, true}]),
-    ets:new(attr_vectors, [public, named_table]),
+    ets:new(semantic_vectors, [public, named_table, {write_concurrency, true}, {read_concurrency, true}]),
     ok.
 
 stop() ->
     ets:delete(index_vectors),
-    ets:delete(attr_vectors),
+    ets:delete(semantic_vectors),
     ok.
 
-
-store_attr_vector(Id, Values) ->
-    ets:insert(attr_vectors, {Id, Values}),
-    ok.
-
-load(table, File) ->
-    csv:parse(file, File, fun store_attr_vector/2),
-    ok;
-load(list, File) ->
-    csv:parse(file, File).
-load(run, File, Fun) ->
-    csv:parse(file, File, Fun),
-    ok.
-
+%%
+%% Run a file
+%%
 run(file, File, Cores, Window, Length, Prob) ->
-    stop(),
-    init(),
-
-    Items = load(list, File),
+    Items = csv:parse(file, File),
     spawn_runner(Cores, Items, Window, Length, Prob);
+
+%%
+%% Run a list of lists
+%%
 run(list, List, Cores, Window, Length, Prob) ->
-    init(),
     spawn_runner(Cores, List, Window, Length, Prob).
 
 
-update_runner(Parent) ->
+%%
+%% Process that updates an item (concurrently)
+%% Receives: {new, Items, Window, Length, Prob} or exit
+%%
+vector_update_process(Parent) ->
     receive
 	{new, {Items, Window, Length, Prob}} ->
 	    update_item(Items, Window, Length, Prob),
-	    update_runner(Parent);
+	    vector_update_process(Parent);
 	exit ->
 	    Parent ! {done, self(), Parent}
     end.
 
+%%
+%% Spawn a Cores number of "vector_update_process"
+%% that can receive Items for processing
+%%
 spawn_runner(Cores, Items, Window, Length, Prob) ->
     Self = self(),
-    Runners = [spawn(?MODULE, update_runner, [Self]) || _ <-  lists:seq(1, Cores)],
-    run_runners(queue:from_list(Runners), Items, Window, Length, Prob),
-    is_done(Self, Cores).
+    Runners = [spawn(?MODULE, vector_update_process, [Self]) || _ <-  lists:seq(1, Cores)],
+    run_vector_update_processes(queue:from_list(Runners), Items, Window, Length, Prob),
+    wait_for_vector_updates(Self, Cores).
 
-is_done(Self, Cores) ->
+wait_for_vector_updates(Self, Cores) ->
     if Cores == 0 ->
 	    ok;
        true ->
 	    receive
 		{done, _, Self} ->
-		    is_done(Self, Cores - 1);
+		    wait_for_vector_updates(Self, Cores - 1);
 		_ -> throw({error, some_error})
 	    end
     end.
 
-run_runners(Runners, [], _, _, _) ->
+%%
+%% Run each Item in Items in a separate process (from Runners).
+%% When length(Items) > length(Runners) each process handle more items
+%% in a "circle"
+%%
+run_vector_update_processes(Runners, [], _, _, _) ->
     [R ! exit || R <- queue:to_list(Runners)];
-run_runners(Runners, [Item|Rest], Window, Length, Prob) ->
+run_vector_update_processes(Runners, [Item|Rest], Window, Length, Prob) ->
     {{value, R}, Queue} = queue:out(Runners),
     R ! {new, {Item, Window, Length, Prob}},
-    run_runners(queue:in_r(R, Queue), Rest, Window, Length, Prob).
-    
+    run_vector_update_processes(queue:in(R, Queue), Rest, Window, Length, Prob).
+   
 
-    
-    
-    
-
+%%
+%% Update Items (a list of Items)
+%%
 update(Items, Window, Length, Prob) ->
     case Items of
 	[] ->
@@ -84,6 +85,10 @@ update(Items, Window, Length, Prob) ->
     end.
 
 
+%%
+%% Update an item, using a window length of Window
+%% a random index vector or Length
+%%
 update_item(Items, Window, Length, Prob) ->
     update_item(Items, Window, Length, Prob, queue:new()).
 update_item(Items, Window, Length, Prob, Queue) ->
@@ -102,6 +107,9 @@ update_item(Items, Window, Length, Prob, Queue) ->
 						    end)
     end.
 
+%%
+%% Update Pivot with all items in Items
+%%
 update_all(Items, Length, Prob, Pivot) ->
     case Items of
 	[] ->
@@ -111,6 +119,9 @@ update_all(Items, Length, Prob, Pivot) ->
 	    update_all(Rest, Length, Prob, Pivot)
     end.
 
+%% 
+%% Update Pivot with Current to Limit number of items
+%%
 update_limit({Limit, Current}, Items, Length, Prob, Pivot) ->
     case Items of
 	[] ->
@@ -124,42 +135,84 @@ update_limit({Limit, Current}, Items, Length, Prob, Pivot) ->
 	    end
     end.
 
-
+%%
+%% Update Pivot w.r.t Item. If index vector for any of the two
+%% does not exist, create it
+%% 
 update_pivot(Pivot, Item, Length, Prob) ->
-   % io:format("Updating: ~p with index vector of ~p ~n", [Pivot, Item]),
-    PivotVector = init_random_vector(Item, Length, Prob),
-    ItemVector  = init_random_vector(Pivot, Length, Prob),
-    ets:insert(index_vectors, {Pivot, vector_addition(PivotVector, ItemVector)}).
+    PivotVector = get_semantic_vector(Item, Length),
+    ItemVector  = get_index_vector(Pivot, Length, Prob),
+    ets:insert(semantic_vectors, {Pivot, vector_addition(PivotVector, ItemVector)}).
 
-init_random_vector(Item, Length, Prob) ->	    
+%%
+%% Init a random vector of Length lenght and the Prob prob to
+%% spawn -1 or 1
+%%
+get_index_vector(Item, Length, Prob) ->	    
     case ets:lookup(index_vectors, Item) of
 	[{_, Vector}] ->
 	    Vector;
 	[] ->
-	    Vector = random_index_vector(Length, Prob),
+	    Vector = new_index_vector(Length, Prob),
 	    ets:insert(index_vectors, {Item, Vector}),
 	    Vector
     end.
-	    
 
+%%
+%% Get the semantic vector for Item (if dont exist, create)
+%%
+get_semantic_vector(Item, Length) ->
+    case ets:lookup(semantic_vectors, Item) of
+	[{_, Vector}] ->
+	    Vector;
+	[] ->
+	    Vector = new_semantic_vector(Length),
+	    ets:insert(semantic_vectors, {Item, Vector}),
+	    Vector
+    end.
+
+%%
+%% Get semantic vector for item
+%%
+get_semantic_vector(Item) ->
+    case ets:lookup(semantic_vectors, Item) of
+	[{_, Vector}] ->
+	    Vector;
+	[] -> throw({error, item_not_found})
+    end.
+
+%%
+%% Create a new semantic vector
+%%
+new_semantic_vector(Length) ->   
+    [0 || _ <- lists:seq(1, Length)].
     
-random_vector(List, Length, Prob) ->
+%%
+%% Create new index vectors for Items in List
+%%
+new_index_vectors(List, Length, Prob) ->
     case List of
 	[] ->
 	    ok;
 	[Item|Rest] ->
-	    ets:insert(index_vectors, {Item, random_index_vector(Length, Prob)}),
-	    random_vector(Rest, Length, Prob)
+	    ets:insert(index_vectors, {Item, new_index_vector(Length, Prob)}),
+	    new_index_vectors(Rest, Length, Prob)
     end.
 
-random_index_vector(Length, Prob) ->
-    random_index_vector(Length, Prob, []).
+%%
+%% Create a new index vector of length Length
+%%
+new_index_vector(Length, Prob) ->
+    new_index_vector(Length, Prob, []).
 
-random_index_vector(0, _, Acc) ->
+new_index_vector(0, _, Acc) ->
     lists:reverse(Acc);
-random_index_vector(Length, Prob, Acc) ->
-    random_index_vector(Length - 1, Prob, [random_index(Prob) | Acc]).
+new_index_vector(Length, Prob, Acc) ->
+    new_index_vector(Length - 1, Prob, [random_index(Prob) | Acc]).
 
+%%
+%% Return -1, 1 or 0
+%%
 random_index(Prob) ->
     X = random:uniform(),
     if
@@ -169,6 +222,12 @@ random_index(Prob) ->
 	    -1;
        true -> 0
     end.
+
+%%
+%% Add two vectors
+%%
+vector_addition(A, B) ->
+    vector_addition(A, B, []).
 
 vector_addition([], [], Acc) ->
     lists:reverse(Acc);
@@ -183,8 +242,11 @@ vector_addition([A|Ar], [B|Br], Acc) ->
 				     true -> 0						  
 				 end | Acc]).
 
-vector_addition(A, B) ->
-    vector_addition(A, B, []).
+%%
+%% Get the cosine similarity between two vectors
+%%
+cosine_similarity(A, B) ->
+    cosine_similarity(A, B, {0, 0, 0}).
 
 cosine_similarity([], [], {Dot, A, B}) ->
     Dot / (math:sqrt(A) * math:sqrt(B));
@@ -194,7 +256,12 @@ cosine_similarity([A|Ar], [B|Br], Acc) ->
     NewLenA = LenA + A * A,
     NewLenB = LenB + B * B,
     cosine_similarity(Ar, Br, {NewDot, NewLenA, NewLenB}).
-cosine_similarity(A, B) ->
-    cosine_similarity(A, B, {0, 0, 0}).
 
 
+%%
+%% Compare two semantic vectors
+%%
+compare_items(A, B) ->
+    SemanticVectorA = get_semantic_vector(A),
+    SemanticVectorB = get_semantic_vector(B),
+    cosine_similarity(SemanticVectorA, SemanticVectorB).
